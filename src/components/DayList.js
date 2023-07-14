df2d246801c4eb7e795b87ff2c73e556f43dacb6
Nanaya <nanayamiki@gmail.com>
2023-07-13 19:35:04 -0700

    update and add props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,5 +1,18 @@
 import React from "react";
+import DayListItem from "./DayListItem";
 
-export default function DayList() {
-	return <ul></ul>;
+export default function DayList(props) {
+	const listItems = props.days.map((day) => {
+		return (
+			<DayListItem
+				key={day.id}
+				name={day.name}
+				spots={day.spots}
+				selected={day.name === props.day}
+				setDay={props.setDay}
+			/>
+		);
+	});
+
+	return <ul>{listItems}</ul>;
 }

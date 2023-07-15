cbe5a1a2947c43766ec909fc46e28fb86b43b91e
Nanaya <nanayamiki@gmail.com>
2023-07-14 19:23:20 -0700

    refactor the componetns and lift the state function to DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,13 +3,16 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
 	const listItems = props.days.map((day) => {
+		const handleClick = () => {
+			props.setDay(props.name);
+		};
 		return (
 			<DayListItem
 				key={day.id}
 				name={day.name}
 				spots={day.spots}
 				selected={day.name === props.day}
-				setDay={props.setDay}
+				setDay={handleClick}
 			/>
 		);
 	});
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,31 +3,17 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-	const handleClick = () => {
-		props.setDay(props.name);
-	};
+	// const handleClick = () => {
+	// 	props.setDay(props.name);
+	// };
 
 	const dayClass = classNames("day-list__item", {
 		" day-list__item--selected": props.selected,
 		" day-list__item--full": props.spots === 0,
 	});
 
-	// const formatSpots = () => {
-	// 	if (props.spots === 0) {
-	// 		return "no spots remaining";
-	// 	} else if (props.spots === 1) {
-	// 		return "1 spot remaining";
-	// 	} else {
-	// 		return props.spots + "spots remaining";
-	// 	}
-	// };
-
-	// const formatSpots = () =>{
-	//   props.spots === 0?"no spots remaining":
-	// }
-
 	return (
-		<li className={dayClass} onClick={handleClick}>
+		<li className={dayClass} onClick={props.setDay}>
 			<h2 className="text--regular">{props.name}</h2>
 			{/* <h3 className="text--light">{props.spots === 0}no spots remaining</h3>
 			<h3 className="text--light">{props.spots === 1} 1 spot remaining</h3>

d5464fec051e61bf2f37651f55a3aded666d0768
Nanaya <nanayamiki@gmail.com>
2023-07-14 19:16:50 -0700

    refactor the component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,24 +1,25 @@
 import React from "react";
-import "./InterviewerListItem.scss";
+import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-	const handleClick = () => {
-		props.setInterviewer(props.id);
-	};
+	// const handleClick = () => {
+	// 	// console.log("onclickcall", props.id);
+	// 	props.setInterviewer(props.setInterviewer);
+	// };
 
 	const interviewerClass = classNames("interviewers__item", {
 		"interviewers__item--selected": props.selected,
 	});
 
 	return (
-		<li className={interviewerClass} onClick={handleClick}>
+		<li className={interviewerClass} onClick={props.setInterviewer}>
 			<img
 				className="interviewers__item-image"
 				src={props.avatar}
-				alt="Sylvia Palmer"
+				alt={props.name}
 			/>
-			{props.name}
+			{props.selected && props.name}
 		</li>
 	);
 }

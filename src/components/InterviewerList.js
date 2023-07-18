5c4a209d86d39be7f3d1a7e42c283c220f06129e
Nanaya <nanayamiki@gmail.com>
2023-07-17 17:35:14 -0700

    delete helper function

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,17 +4,19 @@ import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
 	const interviewerListItems = props.interviewers.map((interviewer) => {
-		const handleClick = () => {
-			// console.log("onclickcall", props.id);
-			props.onChange(interviewer.id);
-		};
+		// const handleClick = () => {
+		// 	// console.log("onclickcall", props.id);
+		// 	props.onChange(interviewer.id);
+		// };
 		return (
 			<InterviewerListItem
 				key={interviewer.id}
 				name={interviewer.name}
 				avatar={interviewer.avatar}
 				selected={interviewer.id === props.value} //3
-				onChange={handleClick}
+				onChange={() => {
+					props.onChange(interviewer.id);
+				}}
 			/>
 		);
 	});

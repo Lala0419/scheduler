004e925acbf3677ccc4fb98ce6cb8be3b801f555
Nanaya <nanayamiki@gmail.com>
2023-07-19 20:38:59 -0700

    add className to let the sstyling display:none work

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,16 +6,16 @@ import Empty from "./Empty";
 
 export default function Appointment(props) {
 	return (
-		<>
+		<div className="appointment">
 			<Header time={props.time} />{" "}
 			{props.interview ? (
-				<Show 
+				<Show
 					student={props.interview.student}
 					interviewer={props.interview.interviewer.name}
 				/>
 			) : (
 				<Empty />
 			)}
-		</>
+		</div>
 	);
 }

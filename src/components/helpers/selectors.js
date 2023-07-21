2cbd9fa313f82606f5913d53497c2a2a1ed43d31
Nanaya <nanayamiki@gmail.com>
2023-07-20 19:48:48 -0700

    add new fucntion getInterview to selectors.js

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -17,6 +17,53 @@ export function getAppointmentsForDay(state, day) {
 	return appointmentsForDay;
 }
 
+// THIS DID NOT WORK BECAUSE INTERVIEWERS IS OBJECTS IN A **OBJECT**
+// export function getInterview(state, interview) {
+// 	if (interview === null) {
+// 		return null;
+// 	}
+
+// 	// const selectedInterviewer = state.interviewers.filter(
+// 	// 	(i) => i.id === interview.interview.interviewer
+// 	// )[0];
+/**
+  selectApp is already done?
+	console.log("selectedInterviewer", selectedInterviewer);
+	Error: .filter not a function
+ */
+
+// }
+
+//pos something code
+/**
+ * インタビューデータが欲しい。。
+	どのデータ？
+	appointんめんtsの中のinterviewえr 番号のやつ
+	then you need to know the ID that matches
+	how do you get the id?
+	answer = interview.interviewer
+	then now you know the interviewer data you want using the id
+
+ */
+
+export function getInterview(state, interview) {
+	if (interview === null) {
+		return null;
+	}
+
+	const interviewerId = interview.interviewer;
+	const selectedInterviewData = state.interviewers[interviewerId];
+
+	return {
+		student: interview.student,
+		interviewer: {
+			id: selectedInterviewData.id,
+			name: selectedInterviewData.name,
+			avatar: selectedInterviewData.avatar,
+		},
+	};
+}
+
 // selectedDay is an object like below
 //  {
 //   "id": 1,
@@ -39,7 +86,7 @@ export function getAppointmentsForDay(state, day) {
 // },
 
 /** 
-if selectedDay = "Monday", appointments array is [1,2,3,4,5]
+if selectedDay.name = "Monday", appointments array is [1,2,3,4,5]
 state.appointments[appointmentID] 
 and this [appointmentID] = 1 or 2 or 3 or 4 or 5
 **/

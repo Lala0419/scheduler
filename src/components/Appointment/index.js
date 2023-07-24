32068aeda92e4391e341794d267847d7d3959661
Nanaya <nanayamiki@gmail.com>
2023-07-24 06:21:24 -0700

    import visualMode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,29 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import { useVisualMode } from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+	const SHOW = "SHOW";
+	const EMPTY = "EMPTY";
+	const CREATE = "CREATE";
+	const { mode, transition, back } = useVisualMode(
+		props.interview ? SHOW : EMPTY
+	);
+	console.log("props.interviewers", props.interviewers);
 	return (
 		<div className="appointment">
 			<Header time={props.time} />{" "}
-			{props.interview ? (
+			{mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+			{mode === SHOW && (
 				<Show
 					student={props.interview.student}
 					interviewer={props.interview.interviewer.name}
 				/>
-			) : (
-				<Empty />
+			)}
+			{mode === CREATE && (
+				<Form interviewers={props.interviewers} onCancel={back} />
 			)}
 		</div>
 	);

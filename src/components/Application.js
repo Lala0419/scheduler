09026811f61bac11991e67a21510058e711227cd
Nanaya <nanayamiki@gmail.com>
2023-07-31 16:20:12 -0700

    move the helper folder out of the components folder.

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import {
 	getAppointmentsForDay,
 	getInterview,
 	getInterviewersForDay,
-} from "./helpers/selectors";
+} from "../helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {

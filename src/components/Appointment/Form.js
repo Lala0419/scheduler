2571bfb335ed1c5a7d24de59051bdd88fd6cf286
Nanaya <nanayamiki@gmail.com>
2023-09-07 20:05:49 -0700

    add new validation func and swap them

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 	const [student, setStudent] = useState(props.student || "");
 	const [interviewer, setInterviewer] = useState(props.interviewer || null);
 	const [errorMsg, setErrorMsg] = useState("");
+	const [error, setError] = useState("");
 
 	const handleChange = (e) => {
 		setStudent(e.target.value);
@@ -13,16 +14,16 @@ export default function Form(props) {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 	};
-	const handleClick = () => {
-		if (student && interviewer) {
-			props.onSave(student, interviewer);
-		} else {
-			setErrorMsg("Please fill your name and choose a interviewer");
-			setTimeout(() => {
-				setErrorMsg("");
-			}, 2000);
-		}
-	};
+	// const handleClick = () => {
+	// 	if (student && interviewer) {
+	// 		props.onSave(student, interviewer);
+	// 	} else {
+	// 		setErrorMsg("Please fill your name and choose a interviewer");
+	// 		setTimeout(() => {
+	// 			setErrorMsg("");
+	// 		}, 2000);
+	// 	}
+	// };
 
 	const reset = () => {
 		setStudent("");
@@ -33,6 +34,20 @@ export default function Form(props) {
 		props.onCancel();
 		reset();
 	};
+
+	function validate() {
+		if (student === "") {
+			setError("Student name cannot be blank");
+			return;
+		}
+
+		// if (interviewer === null) {
+		// 	setError("Please select an interviewer");
+		// 	return;
+		// }
+
+		props.onSave(student, interviewer);
+	}
 	return (
 		<main className="appointment__card appointment__card--create">
 			<section className="appointment__card-left">
@@ -44,8 +59,10 @@ export default function Form(props) {
 						placeholder="Enter Student Name"
 						onChange={handleChange}
 						value={student}
+						data-testid="student-name-input"
 					/>
 				</form>
+				<section className="appointment__validation">{error}</section>
 				<InterviewerList
 					interviewers={props.interviewers}
 					value={interviewer}
@@ -58,7 +75,7 @@ export default function Form(props) {
 					<Button danger onClick={cancel}>
 						Cancel
 					</Button>
-					<Button confirm onClick={handleClick}>
+					<Button confirm onClick={validate}>
 						Save
 					</Button>
 				</section>
